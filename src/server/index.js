2300d3dd0b7bd50e63695acee4ebefa44eeb3250
nguyen tien nghiep <nguyen_tien_nghiep@mediado.jp>
2018-08-21 14:41:04 +0900

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cors from "cors"
+import { renderToString } from "react-dom/server"
+import App from '../shared/App'
+import React from 'react'
+import serialize from "serialize-javascript"
+import { fetchPopularRepos } from '../shared/api'
+import routes from '../shared/routes'
+import { StaticRouter, matchPath } from "react-router-dom"
+const app = express()
+
+app.use(cors())
+
+// We're going to serve up the public
+// folder since that's where our
+// client bundle.js file will end up.
+app.use(express.static("public"))
+
+app.get("*", (req, res, next) => {
+    const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
+
+    const promise = activeRoute.fetchInitialData
+        ? activeRoute.fetchInitialData(req.path)
+        : Promise.resolve()
+
+    promise.then((data) => {
+        const context = { data }
+        const markup = renderToString(
+            <StaticRouter location={req.url} context={context}>
+                <App data={data} />
+            </StaticRouter>
+        )
+
+        res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>SSR with RR</title>
+            <script src="/bundle.js" defer></script>
+            <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+          </head>
+  
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+      `)
+    }).catch(next)
+})
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port: 3000`)
+})
